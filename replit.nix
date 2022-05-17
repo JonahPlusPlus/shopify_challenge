86ca88c97cd4d7134aacf89b2427273b639d31af
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-05-17 15:23:39 -0400

    Correct versioning

--- replit.nix
+++ replit.nix
@@ -1,7 +1,7 @@
 { pkgs }: {
 	deps = [
-        pkgs.ruby_2_7_4
-        pkgs.rubyPackages_2_7_4.solargraph
+        pkgs.ruby_2_7
+        pkgs.rubyPackages_2_7.solargraph
         pkgs.rufo
         pkgs.sqlite
 	];
